31673b65e76f76d4c875e52faf952aa79b9711b2
PJVentures <dreamteam@pjventures.org>
2025-04-10 13:13:28 -0700

    Update local files for PJ Ventures landing page

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+// Mailchimp form validation
+(function($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0]='EMAIL';ftypes[0]='email';
+  fnames[1]='FNAME';ftypes[1]='text';
+  fnames[2]='LNAME';ftypes[2]='text';
+  fnames[3]='ADDRESS';ftypes[3]='address';
+  fnames[4]='PHONE';ftypes[4]='phone';
+  fnames[5]='BIRTHDAY';ftypes[5]='birthday';
+  fnames[6]='COMPANY';ftypes[6]='text';
+}(jQuery));
+var $mcj = jQuery.noConflict(true);
+
 document.addEventListener("DOMContentLoaded", () => {
   // Mobile Menu Toggle with animation
   const menuToggle = document.querySelector(".menu-toggle");
